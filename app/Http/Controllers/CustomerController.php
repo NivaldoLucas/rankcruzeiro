4d2725954c943c8edc9a75b360dc6889766d0443
nivaldo note <nivaldo@grupog30.com.br>
2024-07-15 15:10:27 -0300

    Sorting stars

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -9,7 +9,14 @@ class CustomerController extends Controller
 {
     public function index()
     {
-        $customers = Customer::all();
+        $customers = Customer::all()->sortByDesc(function ($customer) {
+            return ($customer->dobrou_mes1 ? 1 : 0) + 
+                   ($customer->dobrou_mes2 ? 1 : 0) + 
+                   ($customer->referral_1 ? 1 : 0) + 
+                   ($customer->referral_2 ? 1 : 0) + 
+                   ($customer->referral_3 ? 1 : 0);
+        });
+
         return view('customers.index', compact('customers'));
     }
 }
